6766a369d79724961d0b0cf95b820a6ea145ec5f
Sambit Mohanty <Sambit.mohanty954@gmail.com>
2019-02-05 11:33:39 +0530

    First commit

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,42 @@
+class RecipesController < ApplicationController
+	before_action :find_recipe, only: [:show ,:edit ,:update , :destroy]
+	def index
+		@recipe = Recipe.all.order("created_at DESC")
+	end
+	def show
+
+	end
+	def new
+		@recipe = Recipe.new
+	end
+
+	def create
+	   @recipe = Recipe.new(recipe_params)
+		if @recipe.save
+			redirect_to @recipe, notice: "Sucessfully Created New Recipe"
+		else
+			render 'new'
+		end
+	end
+	def edit
+	end
+	def update
+		if @recipe.update_attributes(recipe_params)
+			redirect_to @recipe
+		else
+			render "edit"
+		end
+	end
+		def destroy
+			@recipe.destroy
+			redirect_to root_path, notice: "Sucessfully Deleted The Recipe"
+		end
+
+	private
+	def find_recipe
+		@recipe = Recipe.find(params[:id])
+	end
+	def recipe_params
+		params.require(:recipe).permit(:title, :description, :image, ingredients_attributes:[:id, :name, :_destroy], directions_attributes: [:id,:steps,:_destroy])
+	end
+end
